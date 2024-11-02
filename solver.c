9e87866fc11507d572d4c5008bb1e7ffc41abaa0
jeremlaw <lawrence22097@gmail.com>
2024-11-02 12:32:27 -0400

    updated main

--- solver.c
+++ solver.c
@@ -6,6 +6,7 @@
  *
  **************************************************************/
 #include <stdio.h>
+#include <string.h>
 #include "cube.h"
 #include "moves.h"
 
@@ -13,82 +14,94 @@ int main()
 {   
     printf("\n");
     Cube *cube = newCube();
-    //display(cube);
-    printf("R\n");
-    R(cube);
+    printf("Welcome to the cube simulator! This is your cube:\n\n");
     display(cube);
-    printf("L'\n");
-    Lp(cube);
-    display(cube);
-    printf("B2\n");
-    B2(cube);
-    display(cube);
-    printf("L'\n");
-    Lp(cube);
-    display(cube);
-    printf("B'\n");
-    Bp(cube);
-    display(cube);
-    printf("D\n");
-    D(cube);
-    display(cube);
-    printf("U'\n");
-    Up(cube);
-    display(cube);
-    printf("L2\n");
-    L2(cube);
-    display(cube);
-    printf("B2\n");
-    B2(cube);
-    display(cube);
-    printf("D2\n");
-    D2(cube);
-    display(cube);
-    printf("U'\n");
-    Up(cube);
-    display(cube);
-    printf("B'\n");
-    Bp(cube);
-    display(cube);
-    printf("L2\n");
-    L2(cube);
-    display(cube);
-    printf("B\n");
-    B(cube);
-    display(cube);
-    printf("D'\n");
-    Dp(cube);
-    display(cube);
-    printf("L2\n");
-    L2(cube);
-    display(cube);
-    printf("R2\n");
-    R2(cube);
-    display(cube);
-    printf("F'\n");
-    Fp(cube);
-    display(cube);
-    printf("B'\n");
-    Bp(cube);
-    display(cube);
-    printf("L2\n");
-    L2(cube);
-    display(cube);
-    printf("U2\n");
-    U2(cube);
-    display(cube);
-    printf("D'\n");
-    Dp(cube);
-    display(cube);
-    printf("F2\n");
-    F2(cube);
-    display(cube);
-    printf("D\n");
-    D(cube);
-    display(cube);
-    printf("L2\n");
-    L2(cube);
-    
-    display(cube);
+    printf("Enter any move. Type h for help, or q to quit. \n");
+    char s[] = "  ";
+    while (strcmp(s, "q") != 0) {
+        scanf("%s", s);
+        if (strcmp(s, "q") == 0) {
+            printf("Thanks for cubing. Have a nice day!\n");
+            freeCube(cube);
+            return 0;
+        }
+        else if (strcmp(s, "h") == 0) {
+            printf("----------------- Help Center -----------------\n\n");
+            printf("Sorry, we are currently busy. Please leave a\n");
+            printf("message and we be with you as soon as possible.\n\n");
+            printf("-----------------------------------------------\n");
+        }
+        else {
+            if (strcmp(s, "R") == 0) {
+            R(cube);
+            }
+            else if (strcmp(s, "R'") == 0) {
+                Rp(cube);
+            }
+            else if (strcmp(s, "R2") == 0) {
+                R2(cube);
+            }
+            else if (strcmp(s, "L") == 0) {
+                L(cube);
+            }
+            else if (strcmp(s, "L'") == 0) {
+                Lp(cube);
+            }
+            else if (strcmp(s, "L2") == 0) {
+                L2(cube);
+            }
+            else if (strcmp(s, "U") == 0) {
+                U(cube);
+            }
+            else if (strcmp(s, "U'") == 0) {
+                Up(cube);
+            }
+            else if (strcmp(s, "U2") == 0) {
+                U2(cube);
+            }
+            else if (strcmp(s, "D") == 0) {
+                D(cube);
+            }
+            else if (strcmp(s, "D'") == 0) {
+                Dp(cube);
+            }
+            else if (strcmp(s, "D2") == 0) {
+                D2(cube);
+            }
+            else if (strcmp(s, "F") == 0) {
+                F(cube);
+            }
+            else if (strcmp(s, "F'") == 0) {
+                Fp(cube);
+            }
+            else if (strcmp(s, "F2") == 0) {
+                F2(cube);
+            }
+            else if (strcmp(s, "B") == 0) {
+                B(cube);
+            }
+            else if (strcmp(s, "B'") == 0) {
+                Bp(cube);
+            }
+            else if (strcmp(s, "B2") == 0) {
+                B2(cube);
+            }
+            else if (strcmp(s, "M") == 0) {
+                M(cube);
+            }
+            else if (strcmp(s, "M'") == 0) {
+                Mp(cube);
+            }
+            else if (strcmp(s, "M2") == 0) {
+                M2(cube);
+            }
+            else {
+                printf("Oops! Invalid turn. Choose from the following turns:\n\n");
+                printf("R R' R2 L L' L2 U U' U2 D D' D2 F F' F2 B B' B2 M M' M2\n\n");
+                printf("Your current cube:\n\n");
+            }
+            display(cube);
+        }
+    }
     freeCube(cube);
 }
---
+++ white_cross.h
@@ -0,0 +1,12 @@
+/**************************************************************
+ *
+ *                     white_cross.h
+ *
+ *     This file contains an interface that allows the user to
+ *     create a white cross. This is done in two steps:
+ *          Creating the daisy: four white edges surrounding
+ *          the yellow center
+ *          White cross: four white edges in the correct places
+ *          around the white center
+ *
+ **************************************************************/
